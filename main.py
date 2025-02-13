af87efd7d4b5ddfcdf09387efff1f0027e8b1952
hkukend <kendwj@hku.hk>
2025-02-13 14:28:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from dataset import build_dataset, add_preprocessing
 from model import initialize_model, initialize_processor
@@ -18,6 +19,8 @@ def main():
     # Build the dataset
     raw_datasets = build_dataset()
 
+    assert not_change_test_dataset(raw_datasets), "You should not change the test dataset"
+
     # Initialize the image processor
     processor = initialize_processor()
 
@@ -27,22 +30,24 @@ def main():
     # Build the object detection model
     model = initialize_model()
 
-    assert not_change_test_dataset(datasets), "You should not change the test dataset"
-
     # Build and train the model
     trainer = build_trainer(
         model=model,
         processor=processor,
         datasets=datasets,
     )
+
+    start_time = time.time()
     trainer.train()
+    end_time = time.time()
+    print(f"Training time: {(end_time - start_time) / 60:.2f} minutes")
 
     # Evaluate the model on the test dataset
     test_metrics = trainer.evaluate(
         eval_dataset=datasets["test"],
         metric_key_prefix="test",
     )
-    pprint("Test Metrics:", test_metrics)
+    pprint(test_metrics)
 
 
 if __name__ == "__main__":
