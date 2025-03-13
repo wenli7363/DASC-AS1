5c1752397fa0730a8b4d538c37e7aaf64275242a
hkukend <kendwj@hku.hk>
2025-03-13 10:25:18 +0800

    Update utils.py
    Add warn_only=True for some models that have non-deterministic operators.

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ def set_random_seeds():
     random.seed(SEED)
     np.random.seed(SEED)
     torch.manual_seed(SEED)
+    torch.use_deterministic_algorithms(True, warn_only=True)
 
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(SEED)
